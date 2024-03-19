e143996a678c6d5734f59bfca8b4a49d0fa3788f
michele barsotti <queensofnirvanadoors@gmail.com>
2024-03-19 17:55:23 +0100

    quello che ti  pare

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -20,9 +20,9 @@ class FrontController extends Controller
     public function Servizi(){
 
 $services=[
-    ['id'=>'1','name'=>'Servizio 1','prezzo'=>'100$','tempo'=>'2h'],
-    ['id'=>'2','name'=>'Servizio 2','prezzo'=>'200$','tempo'=>'4h'],
-    ['id'=>'3','name'=>'Servizio 3','prezzo'=>'400$','tempo'=>'3h'],
+    ['id'=>'1','name'=>'Servizio 1','prezzo'=>'100$','tempo'=>'2h','img'=>'/img/servizio1.jpg'],
+    ['id'=>'2','name'=>'Servizio 2','prezzo'=>'200$','tempo'=>'4h','img'=>'img\servizio2.webp'],
+    ['id'=>'3','name'=>'Servizio 3','prezzo'=>'400$','tempo'=>'3h','img'=>'img\servizio3.jpg'],
 ];
         return view('dataPage', ['services'=>$services]);
 
@@ -31,9 +31,9 @@ $services=[
     public function Dettaglio($id){
 
         $services=[
-            ['id'=>'1','name'=>'Servizio 1','prezzo'=>'100$','tempo'=>'2h'],
-            ['id'=>'2','name'=>'Servizio 2','prezzo'=>'200$','tempo'=>'4h'],
-            ['id'=>'3','name'=>'Servizio 3','prezzo'=>'400$','tempo'=>'3h'],
+            ['id'=>'1','name'=>'Servizio 1','prezzo'=>'100$','tempo'=>'2h','img'=>'/img/servizio1.jpg'],
+    ['id'=>'2','name'=>'Servizio 2','prezzo'=>'200$','tempo'=>'4h','img'=>'/img/servizio2.webp'],
+    ['id'=>'3','name'=>'Servizio 3','prezzo'=>'400$','tempo'=>'3h','img'=>'/img/servizio3.jpg'],
         ];
         
         foreach ($services as $service){
